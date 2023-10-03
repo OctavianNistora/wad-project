64a9fd01bf70f66d4be962056d47dd610477a664
Octavian-Tiberiu NISTORA <octavian.nistora@gmail.com>
2023-10-03 21:43:27 +0300

    WADP-01 Fixed

--- src/app/page1/page.tsx
+++ src/app/page1/page.tsx
@@ -1,26 +1,17 @@
 import Link from 'next/link'
-
-export function HomePageLink()
-{
-    return <Link href="/">
-        Go to Home Page
-    </Link>
-}
-
-export function Page2Link()
-{
-    return <Link href="/page2">
-        Go to Page2
-    </Link>
-}
-
-export default function Page()
+export default function Page1()
 {
     return (
-        <>
+        <main>
             <h1>Welcome to page1!</h1>
-            <HomePageLink /><br />
-            <Page2Link />
-        </>
+
+            <Link href="/">
+                Go to Home Page
+            </Link><br />
+            
+            <Link href="/page2">
+                Go to Page2
+            </Link>
+        </main>
     )
 }
--- src/app/page2/page.tsx
+++ src/app/page2/page.tsx
@@ -1,26 +1,16 @@
 import Link from 'next/link'
 
-export function HomePageLink()
-{
-    return <Link href="/">
-        Go to Home Page
-    </Link>
-}
-
-export function Page1Link()
-{
-    return <Link href="/page1">
-        Go to Page1
-    </Link>
-}
-
-export default function Page()
+export default function Page2()
 {
     return (
-        <>
+        <main>
             <h1>Welcome to page2!</h1>
-            <HomePageLink /><br />
-            <Page1Link /><br />
-        </>
+            <Link href="/">
+                Go to Home Page
+            </Link><br />
+            <Link href="/page1">
+                Go to Page1
+            </Link><br />
+        </main>
     )
 }
