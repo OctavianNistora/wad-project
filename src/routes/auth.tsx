ce9618f59559227a5f235bf7966696f3f31c0f2c
Octavian-Tiberiu NISTORA <octavian.nistora@gmail.com>
2023-10-11 20:13:06 +0300

    WADP-04 Added registration and authentication

--- src/routes/auth.tsx
+++ src/routes/auth.tsx
@@ -1,19 +1,50 @@
 import { Button, Container, Stack, TextField, Typography } from "@mui/material";
+import { UserCredential, signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
+import { useNavigate } from "react-router-dom";
 
 export default function Auth() {
+  const navigate = useNavigate();
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    signInWithEmailAndPassword(
+      auth,
+      data.get("email") as string,
+      data.get("password") as string
+    )
+      .then((userCredential: UserCredential) => {
+        const user = userCredential.user;
+        console.log(user);
+        navigate("/homepage");
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+      });
+  };
   return (
     <Container component="main" maxWidth="xs" disableGutters>
       <Stack alignItems="center" spacing={2}>
         <Typography variant="h2" align="center" gutterBottom color={"cyan"}>
           Event Planner
         </Typography>
-        <Stack alignItems="center" spacing={2}>
+        <Stack
+          component="form"
+          onSubmit={handleSubmit}
+          alignItems="center"
+          spacing={2}
+        >
           <TextField
-            label="Email"
+            label="Email Address"
             required
             fullWidth
             autoComplete="email"
             type="email"
+            autoFocus
+            id="email"
+            name="email"
           />
           <TextField
             label="Password"
@@ -21,13 +52,24 @@ export default function Auth() {
             fullWidth
             autoComplete="current-password"
             type="password"
+            id="password"
+            name="password"
           />
-          <Button variant="contained" color="primary" type="submit">
-            Log In
-          </Button>
-          <Button variant="contained" color="secondary" type="button">
-            Sign Up
-          </Button>
+          <Stack direction="row" spacing={2}>
+            <Button variant="contained" color="primary" type="submit">
+              Log In
+            </Button>
+            <Button
+              variant="contained"
+              color="secondary"
+              type="button"
+              onClick={() => {
+                navigate("/register");
+              }}
+            >
+              Sign Up
+            </Button>
+          </Stack>
         </Stack>
       </Stack>
     </Container>
